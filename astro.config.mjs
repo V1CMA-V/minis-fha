4c936a192ddb195d4046e456c64536bec94d255c
Victor Martinez <vicmarc30@gmail.com>
2024-03-23 22:17:49 -0600

    Add @astrojs/node dependency and configure adapter in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node'; // Importa el adaptador
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server"
+  output: "server",
+  adapter: node(), // Configura el adaptador,
+  mode: 'ssr'
 });
