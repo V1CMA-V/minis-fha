c7e0df9bad19a6759daebf146d3948582228bf13
Victor Martinez <vicmarc30@gmail.com>
2024-03-23 22:25:54 -0600

    Update Astro config to use standalone mode for the Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import node from '@astrojs/node'; // Importa el adaptador
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: node(), // Configura el adaptador,
-  mode: 'ssr'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
