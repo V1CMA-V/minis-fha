806c70fe534f6cdecbd4e6a386a2937c6be72158
Victor Martinez <vicmarc30@gmail.com>
2024-03-23 23:02:08 -0600

    Remove unused imports and commented code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node'; // Importa el adaptador
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  
 });
