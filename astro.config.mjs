0b65a5b54f925fac4ef4401ff9abe2f0cdae1dfc
Victor Martinez <vicmarc30@gmail.com>
2024-05-15 19:07:21 -0600

    add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
+import db from "@astrojs/db";
+
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import vercel from "@astrojs/vercel/serverless";
-
 import auth from "auth-astro";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), auth()],
+  prefetch: true,
+  // devToolbar: {
+  //   enabled: false,
+  // },
+  integrations: [tailwind(), preact(), auth(), db()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 });
